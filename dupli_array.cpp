2ce383ccf473abae9b620ff268aa5db1d9ba81d5
AJAMEY77 <138602395+AJAMEY77@users.noreply.github.com>
2023-09-24 22:29:48 +0530

    Add files via upload

---
+++ dupli_array.cpp
@@ -0,0 +1,65 @@
+//Given an array a of size N which contains elements from 0 to N-1, you need to find all the elements occurring more than once in the given array. Return the answer in ascending order. If no such element is found, return list containing [-1]. 
+//
+//Note: The extra space is only for the array to be returned. Try and perform all operations within the provided array. 
+//
+//Example 1:
+//
+//Input:
+//N = 4
+//a[] = {0,3,1,2}
+//Output: 
+//-1
+//Explanation: 
+//There is no repeating element in the array. Therefore output is -1.
+//Example 2:
+//
+//Input:
+//N = 5
+//a[] = {2,3,1,2,3}
+//Output: 
+//2 3 
+//Explanation: 
+//2 and 3 occur more than once in the given array.
+
+
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    vector<int> duplicates(int arr[], int n) {
+        // code here
+         map<int,int> mp;
+    for(int i = 0; i < n; i++)
+        mp[arr[i]]++;
+    vector<int> vec = {};
+    for(auto it : mp)
+        if(it.second > 1)
+            vec.push_back(it.first);
+    if(vec.size()) return vec;
+    return {-1};
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t-- > 0) {
+        int n;
+        cin >> n;
+        int a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+        Solution obj;
+        vector<int> ans = obj.duplicates(a, n);
+        for (int i : ans) cout << i << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
