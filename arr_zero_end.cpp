9a03758890f7e690ca3aeb554d0b2b625c4c9e3b
AJAMEY77 <138602395+AJAMEY77@users.noreply.github.com>
2023-10-31 20:21:41 +0530

    Add files via upload

---
+++ arr_zero_end.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	void pushZerosToEnd(int arr[], int n) {
+	    // code here
+	    int non_zero=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]!=0)
+	        {
+	            non_zero++;
+	        }
+	    }
+	    
+	    int pos=0;
+	    for(int i=0;i<n;i++)
+	    {
+	        if(arr[i]!=0)
+	        {
+	            arr[pos]=arr[i];
+	            pos++;
+	        }
+	    }
+	    
+	    for(int i=non_zero;i<n;i++)
+	    {
+	        arr[i]=0;
+	    }
+	    
+	}
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        int arr[n];
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.pushZerosToEnd(arr, n);
+        for (i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
